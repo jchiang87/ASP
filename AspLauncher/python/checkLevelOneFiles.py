974641625ae0e82c3baef404ec1c305edfd67f74
cvs2git <>
2009-04-09 03:36:54 +0000

    This commit was manufactured by cvs2svn to create tag 'v1r0p1'.

--- AspLauncher/python/checkLevelOneFiles.py
+++ AspLauncher/python/checkLevelOneFiles.py
@@ -66,12 +66,7 @@ def providesCoverage(tstart, tstop, min_frac=0.70, ft1List='Ft1FileList',
     if not ft1 or not ft2:  # at least one file list is empty
         return False
     gtis = FitsNTuple(ft1, 'GTI')
-    try:
-        check_ft2(gtis, ft2)
-    except RuntimeError, message:
-        print "WARNING"
-        print message
-        print 'gtmktime filter="LIVETIME>0" should be run on these data'
+    check_ft2(gtis, ft2)
     print "Requested tstart, tstop: ", tstart, tstop
     print "GTI range: ", min(gtis.START), max(gtis.STOP)
     total = 0

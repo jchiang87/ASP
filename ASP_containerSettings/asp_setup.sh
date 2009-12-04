43ca6b485e9bc8c46f19031ec82b6221a26f6b26
jrb <>
2009-12-04 02:49:49 +0000

    Separate asp_setup.sh into asp_presetup.sh, asp_postsetup.sh

--- ASP_containerSettings/asp_setup.sh
+++ ASP_containerSettings/asp_setup.sh
@@ -14,7 +14,6 @@ export ORACLE_HOME=/afs/slac/package/oracle/d/linux/11.1.0
 
 asp_db_config=${pipeline_config_root}ASP/db_config
 
-
 asp_python_path=${pipeline_config_root}ASP/python/lib/python2.5/site-packages
 asp_lib_path=${pipeline_config_root}ASP/lib
 GPLtools_path=${pipeline_config_root}GPLtools/asp/python
@@ -22,3 +21,10 @@ GPLtools_path=${pipeline_config_root}GPLtools/asp/python
 export LD_LIBRARY_PATH=${ORACLE_HOME}/lib:${LD_LIBRARY_PATH}:${asp_lib_path}
 
 export PYTHONPATH=${asp_python_path}:${GPLtools_path}:${PYTHONPATH}
+
+# Define these temporarily until we can determine how to set them correctly
+# in SCons
+
+export CALDB=/afs/slac/g/glast/groups/asp/jrbdev/ST-LATEST-2992/data/caldb/data/glast/lat
+export CALDBCONFIG=/afs/slac/g/glast/groups/asp/jrbdev/ST-LATEST-2992/data/caldb/software/tools/caldb.config
+export CALDBALIAS=/afs/slac/g/glast/groups/asp/jrbdev/ST-LATEST-2992/data/caldb/software/tools/alias_config.fits
